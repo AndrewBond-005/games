7cb13c902ee2e211522f3cfd725d3f6c53656d56
AndrewBond-005 <andreivinner2000@gmail.com>
2026-06-08 15:58:31 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/game/:gameId" element={<GameWrapper gameId={'exam-cheat'} />} />
+                <Route path="/game/:gameId" element={<GameWrapper  />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>
